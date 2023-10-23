7d1fed93b9bdbc7873bda195e2c7d0e58a7de17e
Abner Amorim <47486200+abneramorim@users.noreply.github.com>
2023-10-23 23:40:20 +0000

    Remoção da imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <body><h1>Gabrielle, amo você! Se você me ama, dá uma risadinha!</h1><img src="bg-teste.png"></img><br></br></body>;
+    return <h1>Gabrielle, amo você! Se você me ama, dá uma risadinha!</h1>;
 }
 
 export default Home;
